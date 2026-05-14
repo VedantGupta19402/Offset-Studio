e70524ae5be08a69dcc544ccaf8d7cdee72c6a34
VedantGupta19402 <vedantguptav@gmail.com>
2026-05-14 22:18:19 +0530

    working on it

--- src/components/home/Top.jsx
+++ src/components/home/Top.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Top = () => {
   return (
     <>
-     <div className='h-screen w-screen flex flex-between'>
-      <h2>HOME</h2>
-      <h2>AGENCY</h2>
-      <h2>ABOUT</h2>
-      <h2>JOURNAL</h2>
-      <h2>GENRAL</h2>
-      <h2>CONTACT</h2>
-      </div> 
+        <div className='w-screen h-20 flex flex-between absolute top-[8%] uppercase text-[rgba(245,243,238,0.82)] justify-center gap-10 '>    
+      <h2>Home</h2>
+      <h2>Agency</h2>
+      <h2>About</h2>
+      <h2>Journal</h2>
+      <h2>Genral</h2>
+      <h2>Contact</h2>
+      </div>
     </>
   )
 }
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,9 +3,16 @@ import Top from './Top'
 
 const Video = () => {
   return (
-    <div className='bg-cover h-screen w-screen'>
+    <div className='bg-cover h-screen w-screen relative'>
       <video autoPlay loop muted src='\public\video.mp4'></video>
       <Top/>
+      <div>
+        <h1 className='w-screen h-20 text-9xl flex flex-between  justify-center absolute top-[30%]  uppercase text-[#F5F3EE] font-bold '>
+        ideas that <br /> move brands
+          <br />forward
+        </h1>
+      
+      </div>
     </div>
   )
 }
